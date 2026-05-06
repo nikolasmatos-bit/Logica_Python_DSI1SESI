181bcda53c89f73b0d7466171552ec6edda9c525
Nikolas Silva de Matos <nikolas.1silva2@gmail.com>
2026-05-06 11:24:56 -0300

    Meu primeiro comando Python

---
+++ 1.1-numeros-em-pyhton.py
@@ -0,0 +1,31 @@
+# AULA COMPLETA: NUMEROS EM PYHTON
+
+    # """
+    # Vamos Aprender:
+    # 1 - Tipos Numéricos 
+    # 2 - Conversões de Tipos
+    # 3 - Hierarquia Númérica 
+    # 4 - Operações Matemáticas
+    # 5 - Coerção de Tipos
+    # 6 - Verificação de Tipos 
+    # 7 - Entrada de Dados
+    # """
+##############################
+ ## PASSO 01 - TIPOS NUMÉRICOS 
+##############################
+# int -> Números inteiros
+# float -> Números com casas decimais
+# complex -> Númeeros complexos (usado em Matemática/engenharia)
+
+print("===== TIPOS NUMÉRICOS =====")
+ # EXEMPLO 01 - NUMERO INTEIRO 
+
+ #criamos uma varíavel chamada número inteiro
+numero_inteiro = 10
+
+#Mostramos o valor
+print ("Valor:", numero_inteiro)
+#Type() mostra qual é o tipo da variável
+print("TIPO:", type (numero_inteiro))
+
+print("-------------------------")
