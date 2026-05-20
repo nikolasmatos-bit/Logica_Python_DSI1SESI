9d6fbf6fabd271346556a2d19ad29a720a822b4d
Nikolas Silva de Matos <nikolas.1silva2@gmail.com>
2026-05-20 09:17:22 -0300

    Atividade 1 de python

---
+++ 1.1-exercícios-em-python.py
@@ -0,0 +1,111 @@
+# EX1 
+# Um aluno tem 10 anos. Adicione Uma variavel
+# exiba seu tipo
+
+anos = 10
+
+print ("idade do aluno:", anos)
+print("TIPO:", type (anos))
+
+# EX2
+# A temperatura medida é 23.5°C.
+# Armazene esse valor e mostre seu tipo.
+
+temperatura = 23.5 
+
+print ("temperatura do local:" , temperatura)
+print("tipo:" , type(temperatura))
+
+# EX3
+# Crie um número complexo representando uma impedância elétrica
+# de 5 + 8j e mostre sua parte real.
+
+# parte real (Numero Real)
+# Parte Imaginária (multiplicado por J)
+
+# a = parte real
+# b = parte imaginária 
+# j = unidade imaginária 
+
+impendancia_elétrica = 5 + 8j
+
+print("Impedanica.imag")
+
+# EX4
+# Mostre a parte imaginária do número complexo
+# criado no exercício anterior.
+
+Parte_Imaginária = 0j 
+
+print("parte_imaginária.imag")
+
+
+# EX5
+# Declare uma variável chamada "populacao"
+# com o valor 8_000_000_000 (8 bilhões)
+# e mostre seu tipo.
+
+      
+População = 8,000,000,000
+
+print("População", População)
+
+
+
+# EX6
+# Verifique se o número 7 é do tipo int
+# usando a função type().
+
+
+Número = 7 
+
+print("Valor", type(Número))
+
+
+
+
+# EX7
+# Crie uma variável chamada "aprovado"
+# com o valor booleano True e mostre seu tipo.
+
+
+Aprovado = 5 > 3 
+
+print("Aprovado", Aprovado)
+
+
+
+
+
+# EX8
+# Some True e False e mostre o resultado
+# e também o tipo do resultado.
+
+
+Valor = 5 > 3 + 10 < 2
+
+print("Valor", Valor)
+
+
+
+
+
+
+# EX9
+# Pesquise e mostre qual é o valor máximo
+# que um número inteiro pode ter em Python.
+
+
+Numero = 1000000000000000000000000000000000000000000000000000000000000000000000
+
+print("Numero", Numero)
+
+
+# EX10
+# Mostre a representação em binário
+# do número 10 usando uma função do Python.
+
+
+numero_binario  =   "0101"
+
+print("numero_binario", numero_binario) 
